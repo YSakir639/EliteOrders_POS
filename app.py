1fa1b5e23624a8fa55b89d31c8e3fa3ed13a34ff
YSakir639 <103224018+YSakir639@users.noreply.github.com>
2024-03-18 12:36:28 +0000

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from EliteOrders import create_app
 from EliteOrders.events import socketio
 # assign a variable to store create_app
 # Import the SocketIO class from the flask_socketio module
+from flask_socketio import SocketIO
 from engineio.async_drivers import threading
 app = create_app()
 
@@ -13,7 +14,7 @@ socketio.init_app(app,async_mode="threading")
 
 # check weather the app is running from the main file 
 if __name__=="__main__":
-   socketio.run(app,host="0.0.0.0",port=80)
+   socketio.run(app,host="0.0.0.0",port=80,debug=True)
    # app.run(host="0.0.0.0",port=80)
     
 
